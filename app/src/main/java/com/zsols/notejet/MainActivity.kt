01e0bc92bdc5e5bd39a89906546c11ec151ad9fb
zakiSiddiqui <msiddiqui@i2cinc.com>
2023-01-31 18:41:22 +0500

    Dependency Injection part 1

--- app/src/main/java/com/zsols/notejet/MainActivity.kt
+++ app/src/main/java/com/zsols/notejet/MainActivity.kt
@@ -3,6 +3,7 @@ package com.zsols.notejet
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -18,14 +19,17 @@ import com.zsols.notejet.model.NoteData
 import com.zsols.notejet.screen.NoteScreen
 import com.zsols.notejet.screen.NoteViewModel
 import com.zsols.notejet.ui.theme.NoteJetTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             NoteJetTheme {
                 // A surface container using the 'background' color from the theme
-                NoteApp()
+                val noteViewModel: NoteViewModel by viewModels()
+                NoteApp(noteViewModel = noteViewModel)
             }
         }
     }
@@ -33,17 +37,18 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun NoteApp(noteViewModel: NoteViewModel = viewModel()){
+
+    val notesList = noteViewModel.getAllNotes()
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colors.background
     ) {
-        val notes = remember {
+/*        val notes = remember {
             mutableStateListOf<NoteData>()
-        }
-        NoteScreen(notes = notes , onAddNote = {
-            notes.add(it)}, onRemoveNote = {
-                notes.remove(it)
-        })
+        }*/
+        NoteScreen(notes = notesList ,
+            onAddNote = { noteViewModel.addNote(it)},
+            onRemoveNote = { noteViewModel.removeNote(it) })
     }
 }
 
